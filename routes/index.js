b71c8ae8eac81fe840d55a10c5da6b94274aab18
Mika Yeap <mikaomarr@gmail.com>
2017-05-09 22:37:59 +0800

    Added GET routes for all todo, category, priority

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
-var Request = require('tedious').Request;  
 var TYPES = require('tedious').TYPES; 
 
 // Create connection to database
@@ -31,20 +30,28 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Welcome' });
 });
 
-router.route('/todos/:userId')
-    .get(function (req, res, next) {
+// GET todos 
+router.get('/all/todo/:userId', generateGET('todo', 'userId'));
+
+// GET categories 
+router.get('/all/category/:userId', generateGET('category', 'userId'));
+
+// GET priorities 
+router.get('/all/priority/:userId', generateGET('priority', 'userId'));
+
+function generateGET(table, matchId) {
+    return function (req, res, next) {
         console.log('GET received');
-        var request = new Request(`SELECT * FROM todo WHERE person_id = ${ req.params['userId'] }`, 
+        var request = new Request(generateQuery(table, req.params[matchId]), 
             function(err) {  
                 if (err) {  
                     console.log(err);}  
-                });
+                }
+            );
 
         // Build array of json objects to return
         var jsonArray = [];
         request.on('row', function(columns) {  
-            console.log('hit row!');
-            console.log(jsonArray.length);
             var rowObject = {};
             columns.forEach(function(column) {  
                 rowObject[column.metadata.colName] = column.value;
@@ -58,6 +65,20 @@ router.route('/todos/:userId')
         });
 
         connection.execSql(request);
-    });
+    }
+}
+
+function generateQuery(table, id) {
+    if (table) {
+        return `SELECT * FROM ${ table } WHERE person_id = ${ id }`;
+    } else {
+        return null;
+    }
+};
+
+// POST todo
+router.post('/todo/:userId/:category*?', function(req, res, next) {
+
+});
 
 module.exports = router;
