94601e982c6052c9fd753b341d30509109fb38d5
Mika Yeap <mikaomarr@gmail.com>
2017-05-21 17:59:39 +0800

    Removed unnecessary types

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
-var TYPES = require('tedious').TYPES; 
 
 // Create connection to database
 var config = {
