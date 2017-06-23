b5fcf2f636abbfb15ab09cd43238160f56e5aa19
Mika Yeap <mikaomarr@gmail.com>
2017-06-23 19:22:25 +0800

    AuthO

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
 let express = require('express');
 let router = express.Router();
 
-
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 
@@ -25,6 +24,44 @@ connection.on('connect', function (err) {
 		console.log("Connected");
 	}
 });
+
+// AuthO JWT validation 
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const jwtAuthz = require('express-jwt-authz');
+
+// Authentication middleware. When used, the
+// access token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and 
+  // the singing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://porcupine.au.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'http://porcupine-dope-api.azurewebsites.net',
+  issuer: `https://porcupine.au.auth0.com/`,
+  algorithms: ['RS256']
+});
+
+// protecting endpoint example: only access token with scope 'read:messages' can access
+/*
+const checkScopes = jwtAuthz([ 'read:messages' ]);
+
+app.get('/api/private', checkJwt, checkScopes, function(req, res) {
+  res.json({ 
+    message: "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this." 
+  });
+});
+*/
+
+router.use(checkJwt);
 
 // ROUTES
 
