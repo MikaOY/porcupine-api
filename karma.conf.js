4e14ca372f229e06d32f21ce15a411fed95da3bf
Mika Yeap <mikaomarr@gmail.com>
2017-07-04 09:40:11 +0800

    Getting karma and requrie to work

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs', 'bable'],
+    frameworks: ['jasmine', 'requirejs', 'babel'],
 
 
     // list of files / patterns to load in the browser
@@ -30,7 +30,15 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+			'./routes/*.spec.js': ['babel']
     },
+
+		plugins: [
+			'karma-babel-preprocessor',
+			'karma-jasmine',
+			'karma-chrome-launcher',
+			"karma-requirejs"
+		],
 
 
     // test results reporter to use
